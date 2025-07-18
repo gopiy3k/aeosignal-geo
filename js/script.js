dcba9d7b35447c371d214f7b49b2cbdf460c01db
gopiy3k <gopiy3k@gmail.com>
2025-07-18 23:49:15 +0530

    h

--- js/script.js
+++ js/script.js
@@ -319,7 +319,19 @@ accordionToggles.forEach(toggle => {
         }
     });
 });
+const backToTop = document.getElementById('backToTop');
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none';
+    }
+});
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
     // Start observing the body for attribute changes
     observer.observe(body, { attributes: true });
