07116a05c6a0729fc7fc69cc3c4b0a3aec5cc1e2
gopiy3k <gopiy3k@gmail.com>
2025-08-01 20:52:24 +0530

    b

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("favicon.png");
     eleventyConfig.addPassthroughCopy("images/**/*");
     eleventyConfig.addPassthroughCopy("robots.txt");
-    eleventyConfig.addFilter("date", function(dateObj, format = "DDD") {
+    eleventyConfig.addPassthroughCopy("blog");
+        eleventyConfig.addFilter("date", function(dateObj, format = "DDD") {
         return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
 });// Split filter for Nunjucks (needed for schema breadcrumb)
 eleventyConfig.addNunjucksFilter("split", function(str, separator) {
